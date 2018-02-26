1f859c488a55a6225fb4b08b8266f806e02cf589
hoolahoop <jrmy.millar@gmail.com>
2018-02-26 09:16:01 -0500

    Forgot to save notes file.

--- Karen/blog_reload/README.md
+++ Karen/blog_reload/README.md
@@ -7,6 +7,12 @@ Things you may want to cover:
 
 * Ruby version
 
+Command line: ruby -v
+Command line: ruby 2.4.1p111 (2017-03-22 revision 58053) [x86_64-linux]
+
+Command line: rails -v
+Command line: Rails 5.1.5
+
 * System dependencies
 
 * Configuration
--- Maestro/blog/app/models/event.rb
+++ Maestro/blog/app/models/event.rb
@@ -7,6 +7,6 @@ class Event < ApplicationRecord
   validates :apartment_number, presence: false, numericality: { only_integer: true }, length: { maximum: 10 }
   validates :city, length: { maximum: 100 }
   validates :date, format: { with: /\A[2][0-9]{3}\-[0-1][0-9]\-[0-3][0-9]\z/, message: "Enter proper numbers please. Year must be after 1999." }
-  validates :time, format: { with: /\A[0-2][0-9]:[0-5][0-9]\z/, message: "Enter proper format please. " }  #[-\+][0-9]{4} or [0-9]{4}-[0-9]{2}-[0-9]{2}[0-2][0-9]:[0-5][0-9]:[0-5][0-9] UTC
+  validates :time, allow_nil: true, format: { with: /\A[0-2][0-9]:[0-5][0-9]\z/, message: "Enter proper format please. " }  #[-\+][0-9]{4} or [0-9]{4}-[0-9]{2}-[0-9]{2}[0-2][0-9]:[0-5][0-9]:[0-5][0-9] UTC
   validates :name, presence: true, length: { minimum: 5, maximum: 100 }
 end
