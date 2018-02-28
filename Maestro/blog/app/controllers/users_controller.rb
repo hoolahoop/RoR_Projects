00a59cf7fc95ef9fcd1fcc8fcbc509914bc96795
hoolahoop <jrmy.millar@gmail.com>
2018-02-28 17:02:10 -0500

    User controller destroy action written. Need to test.

--- Maestro/blog/app/controllers/users_controller.rb
+++ Maestro/blog/app/controllers/users_controller.rb
@@ -7,12 +7,7 @@ class UsersController < ApplicationController
 	end
 
 	def display
-		#@event = Event.find(params[:event_id])
-		#@users = EventUser.joins(params[event_id: :event_id])
 		@users = User.joins("INNER JOIN events_users ON events_users.user_id = users.id AND events_users.event_id = #{params[:event_id]}")
-
-		#@users_events = @events_users.where(event_id: :event_id)
-		#@users = @users.where
 	end
 
 	def new
@@ -26,10 +21,16 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:article_id])
-		@comment = @article.comments.find(params[:id])
-		@comment.destroy
-		redirect_to article_path(@article)
+		#@article = Article.find(params[:article_id])
+		#@comment = @article.comments.find(params[:id])
+		#@comment.destroy
+
+		@event = Event.find(params[:event_id])
+		@user = User.find(params[:id])
+		@join = EventUser.find(params[event_id: :event_id, user_id: :id])
+		@join.destroy
+		
+		redirect_to event_display_path(@event)
 	end
 
 	private
